1f2527fdbcbbfb101bcfbcd15d9e64171b89f958
jawadahmedshahani <70814895+jawadahmedshahani@users.noreply.github.com>
2021-09-09 15:12:48 -0700

    taylor series using recursion

---
+++ taylor series/taylor series/taylor series.cpp
@@ -0,0 +1,39 @@
+// taylor series.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+double e(int m, int n)
+{
+    static double p =1, f = 1;
+    double r;
+    if (n == 0)
+        return 1;
+    else
+    {
+        r = e(m, n - 1);
+        p = p * m;
+        f = f * n;
+        return r + p / f;
+    }
+
+}
+
+int main()
+{ 
+    int x = 1;
+    int y = 10;
+    double r = e(x, y);
+    std::cout << "the value of r is =" << r << "\n";
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
